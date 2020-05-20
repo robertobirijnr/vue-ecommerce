e94fd7c10ea3d1b96b5a870212e8ccaf3a35fc95
robertobirijnr <robertobirijnr@hotmail.com>
2020-05-20 18:24:58 +0100

    navbar

---
+++ src/Pages/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <Navbar></Navbar>
+</template>
+
+<script>
+import Navbar from "../components/Navbar";
+export default {
+  name: "Vue-Ecommerce",
+  components: {
+    Navbar
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+body {
+  font-family: "Montserrat";
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="container">
+    <nav class="mynvabar navbar navbar-expand-lg navbar-light">
+      <a class="navbar-brand" href="#">
+        <span class="o-color">Shop pye</span>
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">
+              PROMOS
+              <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">CATEGORIES</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">MERCHANTS</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">MY CART</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</template>
+<style  scoped>
+.mynvabar .navbar-brand {
+  font-weight: bold;
+  font-size: 30px;
+  color: #5c5c5c;
+}
+
+.mynvabar .navbar-nav {
+  margin-top: 10px;
+}
+
+.mynvabar .navbar-nav li {
+  padding-right: 20px;
+}
+
+.mynvabar .navbar-nav li a {
+  color: #5c5c5c;
+}
+.o-color {
+  color: #ffbb5d;
+}
+</style>
